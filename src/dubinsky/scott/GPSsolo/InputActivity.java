ae15e06157ca9f926bcfc3ec3e00c8cc53b119de
Scott Dubinsky <smdubinsky@gmail.com>
2016-01-07 01:14:31 -0500

    Set front page to test relative azimuth finding

--- src/dubinsky/scott/GPSsolo/InputActivity.java
+++ src/dubinsky/scott/GPSsolo/InputActivity.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class InputActivity extends Activity {
     public final static String LAT = "dubinsky.scott.latitude";
     public final static String LON = "dubinsky.scott.longitude";
+    private final double baseLat = 0.0;
+    private final double baseLon = 0.0;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -20,7 +23,7 @@ public class InputActivity extends Activity {
 
     public void onClick(View view){
         Context context = getApplicationContext();
-        String text = "Yay!  You clicked the button!";
+        String text = "You clicked the button!";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
@@ -30,10 +33,14 @@ public class InputActivity extends Activity {
         double latitude = Double.parseDouble(lat.getText().toString());
         double longitude = Double.parseDouble(lon.getText().toString());
 
-        Intent displayBearing = new Intent(this, DirectionActivity.class);
-        displayBearing.putExtra(LAT, latitude);
-        displayBearing.putExtra(LON, longitude);
+        double relativeAzimuth = RelativeAzimuth.calculateAzimuth(baseLat, baseLon, latitude, longitude);
 
-        startActivity(displayBearing);
+        TextView azimuth = (TextView) findViewById(R.id.calculatedAzimuth);
+        azimuth.setText(Double.toString(relativeAzimuth));
+//        Intent displayBearing = new Intent(this, DirectionActivity.class);
+//        displayBearing.putExtra(LAT, latitude);
+//        displayBearing.putExtra(LON, longitude);
+//
+//        startActivity(displayBearing);
     }
 }
